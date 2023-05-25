92d8901c0a359e0a81fc4675e20a4a9a41433bd3
Swapnil Wakchaure <swapnilwakchaure2411999@gmail.com>
2023-05-25 16:23:30 +0530

    navbar work

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,37 @@
+import "../Styles/navbar.css";
+
+const Navbar = () => {
+    return (
+        <div className="main-navbar">
+            <div className="main-logo">
+                <img src="https://www.transportsimple.com/redesign/images/logo.png" alt="transportsimple-logo" />
+            </div>
+            <div className="main-menu">
+                <ul>
+                    <li>
+                        <a href="">Home</a>
+                    </li>
+                    <li>
+                        <a href="">Feature</a>
+                    </li>
+                    <li>
+                        <a href="">Industry</a>
+                    </li>
+                    <li>
+                        <a href="">About</a>
+                    </li>
+                    <li>
+                        <a href="">Contact us</a>
+                    </li>
+                </ul>
+            </div>
+            <div className="main-demo">
+                <a href="">
+                    <button>Get Demo</button>
+                </a>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
